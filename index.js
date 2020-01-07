23fecb7334aeaca3f89d3ac0e73ba75691469ff8
antonio.abreu <antonio.abreu@virtus.ufcg.edu.br>
2020-01-07 17:10:00 -0300

    finalizado back-end

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
+require('dotenv').config();
 const aws = require('aws-sdk');
 var express = require('express');
 var app = express();
-require('dotenv').config();
 const bodyParser = require('body-parser');
-const cors       = require('cors');
-(async function() {
+const cors = require('cors');
 
-    try {
+aws.config.setPromisesDependency();
+aws.config.update({
+    accessKeyId: process.env.ACCESSKEY,
+    secretAccessKey: process.env.SECRETKEY,
+    region: process.env.REGION
+}) 
 
-    aws.config.setPromisesDependency();
-    aws.config.update({
-        accessKeyId: process.env.ACCESSKEY,
-        secretAccessKey: process.env.SECRETKEY,
-        region: process.env.REGION
-    }) 
-
-    var params = {
-        LanguageCode: 'pt',
-        TextList: ["minha impressora Canon Maxx Tinta G3111 parou de funcionar e só mostra uma luz vermelha no painel"]
-    };
-
-    var comprehend = new aws.Comprehend();
-    comprehend.batchDetectKeyPhrases(params, (err, data) => {
-        if(!err) {
-            console.log("\npalavras chave:\n")
-            console.log(data.ResultList[0].KeyPhrases)
-        } else {
-            console.log(err)
-        }
-    });
-
-    comprehend.batchDetectEntities(params, (err, data) => {
-        if(!err) {
-            console.log("\nElementos encontrados:\n")
-            console.log(data.ResultList[0].Entities)
-        } else {
-            console.log(err)
-        }
-    });
-
-    comprehend.batchDetectSentiment(params, (err, data) => {
-        if(!err) {
-            console.log("\nSentimentos encontrados:\n")
-            console.log(data.ResultList[0])
-        } else {
-            console.log(err)
-        }
-    })
-
-    } catch(e) {
-        console.log('our error', e);
-    }
-    
-})();
+var comprehend = new aws.Comprehend();
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.post('/', async function (req, res) {
+    var response = {}
+    cont = 0;
+    
+    var params = {
+        LanguageCode: 'pt',
+        TextList: [req.body.phrase]
+    };
+
+    await comprehend.batchDetectKeyPhrases(params, (err, data) => {
+        if(!err) {
+            response.KeyPhrases = data.ResultList[0].KeyPhrases;
+            cont ++
+            if(cont === 3) {
+                res.send(response);
+            }
+        } else {
+            console.log(err)
+        }
+    });
+
+    await comprehend.batchDetectEntities(params, (err, data) => {
+        if(!err) {
+            response.Entities = data.ResultList[0].Entities;
+            cont ++
+            if(cont === 3) {
+                res.send(response);
+            }
+        } else {
+            console.log(err)
+        }
+    });
+
+    await comprehend.batchDetectSentiment(params, (err, data) => {
+        if(!err) {
+            response.Sentiment = data.ResultList[0];
+            cont ++
+            if(cont === 3) {
+                res.send(response);
+            }
+        } else {
+            console.log(err)
+        }
+    });
 });
 
-app.post('/', function (req, res) {
-    
-    res.send({response: req.body});
-});
   
 
 app.listen(3000, function () {
